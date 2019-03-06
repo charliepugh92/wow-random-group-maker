5e7d9f11aa34f7404e44b8537841e68b32655763
Charlie Pugh <charliepugh92@gmail.com>
2019-03-05 20:14:31 -0500

    make the group generator smarter, to pick people that are only one spec before people that fill multiple

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,28 +1,89 @@
 class Character < ApplicationRecord
-  def self.generate_groups
-    number_of_groups = Character.count / 5
-    groups = []
-    number_of_groups.times do
-      groups.push({})
+  class << self
+    def generate_groups
+      number_of_groups = Character.count / 5
+
+      tanks = get_tanks(number_of_groups)
+
+      healers = get_healers(number_of_groups, tanks.map(&:id))
+
+      dps = get_dps(number_of_groups, tanks.map(&:id) + healers.map(&:id))
+
+      create_groups(tanks, healers, dps)
     end
 
-    tanks = Character.where(tank: true).order('RANDOM()').limit(number_of_groups)
-    tanks.each_with_index do |tank, i|
-      groups[i][:tank] = tank
+    def create_groups(tanks, healers, dps)
+      groups = []
+      
+      tanks.count.times do |i|
+        groups.push({
+          tank: tanks[i],
+          healer: healers[i],
+          dps: dps[i]
+        })
+      end
+
+      groups
     end
 
-    healers = Character.where(healer: true).where.not(id: tanks.map(&:id)).order('RANDOM()').limit(number_of_groups)
-    healers.each_with_index do |healer, i|
-      groups[i][:healer] = healer
+    def get_tanks(count, already_assigned_ids = [])
+      tanks = []
+
+      tank_only = Character.where(tank: true, healer: false, dps: false).where.not(id: already_assigned_ids).order('RANDOM()')
+      tank_only.each do |tank|
+        break if tanks.count == count
+        tanks.push tank
+      end
+      
+      if tanks.count < count
+        other_tanks = Character.where(tank: true).where.not(id: tank_only.map(&:id)).where.not(id: already_assigned_ids).order('RANDOM()')
+        other_tanks.each do |tank|
+          break if tanks.count == count
+          tanks.push tank
+        end
+      end
+
+      tanks.shuffle
     end
 
-    dps = Character.where(dps: true).where.not(id: tanks.map(&:id)).where.not(id: healers.map(&:id)).order('RANDOM()').limit(number_of_groups * 3)
-    dps.each_with_index do |dpser, i|
-      assigned_group = i % number_of_groups
-      groups[assigned_group][:dps] = [] unless groups[assigned_group][:dps]
-      groups[assigned_group][:dps].push dpser
+    def get_healers(count, already_assigned_ids = [])
+      heals = []
+
+      heal_only = Character.where(tank: false, healer: true, dps: false).where.not(id: already_assigned_ids).order('RANDOM()')
+      heal_only.each do |healer|
+        break if heals.count == count
+        heals.push healer
+      end
+      
+      if heals.count < count
+        other_heals = Character.where(healer: true).where.not(id: heal_only.map(&:id)).where.not(id: already_assigned_ids).order('RANDOM()')
+        other_heals.each do |healer|
+          break if heals.count == count
+          heals.push healer
+        end
+      end
+
+      heals.shuffle
     end
-    
-    groups
+
+    def get_dps(count, already_assigned_ids = [])
+      dps = []
+
+      dps_only = Character.where(tank: false, healer: false, dps: true).where.not(id: already_assigned_ids).order('RANDOM()')
+      dps_only.each do |tank|
+        break if dps.count == count * 3
+        dps.push tank
+      end
+
+      if dps.count < count * 3
+        other_dps = Character.where(dps: true).where.not(id: dps_only.map(&:id)).where.not(id: already_assigned_ids).order('RANDOM()')
+        other_dps.each do |dpser|
+          break if dps.count == count * 3
+          dps.push dpser
+        end
+      end
+
+      dps.shuffle.each_slice(count).to_a
+    end
   end
 end
