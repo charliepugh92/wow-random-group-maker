f8aa6fec8c5321a23a951856256b81afdf695f30
Charlie Pugh <charliepugh92@gmail.com>
2019-08-31 15:35:04 -0400

    initialize invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :invitable,
+  devise :invitable, :database_authenticatable, :invitable,
          :recoverable, :rememberable, :validatable
 end
