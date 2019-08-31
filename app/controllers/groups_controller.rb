6c59f8bbb0287fea8a861435b7cd8aba36d992bf
Charlie Pugh <charliepugh92@gmail.com>
2019-08-31 19:40:43 -0400

    Attempt to prevent rails from caching the groups page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < AuthenticatedController
   def show
     @groups = Character.generate_groups
+
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
